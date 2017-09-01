5b30f30d50565fa99eb3246c14aef5aa57a80457
Geek.Yu <1098654043@qq.com>
2017-09-01 16:28:48 +0800

    出了问题

---
+++ nuxt.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  head: {
+    titleTemplate: '%s - 木有车',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '这是一个页面描述，有利于seo' }
+    ]
+  }
+}
